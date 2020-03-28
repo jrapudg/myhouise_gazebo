854987321bd2f6f810f5a265345612aad8721ee7
Juan Alvarez <jrap.udg@gmail.com>
2020-03-27 22:23:14 -0600

    uploading to remote

---
+++ script/welcome_message.cpp
@@ -0,0 +1,19 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Hello, You are very wellcome to my house!\n");
+	      printf("Guadalajara, Mexico is awesome!\n");
+	      printf("Feel like at home!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
